3ce56e2c079b94e174488a85143dadad475fa3df
Duda.Araujo <eduarda.mpenas@gmail.com>
2026-02-05 21:18:50 -0300

    Testess

--- PopupBackgroundApp.Tests/Core/AuthServiceTests.cs
+++ PopupBackgroundApp.Tests/Core/AuthServiceTests.cs
@@ -1,10 +1,9 @@
-﻿
-
- using NUnit.Framework;
+﻿using NUnit.Framework;
 using Moq;
 using PopupBackgroundApp.Core;
 using PopupBackgroundApp.Interfaces;
 using PopupBackgroundApp.Models;
+using System;
 using System.Threading.Tasks;
 
 namespace PopupBackgroundApp.Tests.Core
@@ -13,12 +12,12 @@ namespace PopupBackgroundApp.Tests.Core
     public class AuthServiceTests
     {
         [Test]
-        public async Task ObterBearerTokenAsync_DeveRetornarToken()
+        public async Task ObterBearerTokenAsync_DeveRetornarToken_QuandoApiResponderComSucesso()
         {
             var authApiMock = new Mock<IAuthApi>();
 
             authApiMock
-                .Setup(x => x.GerarTokenAsync(It.IsAny<OAuthRequest>()))
+                .Setup(api => api.GerarTokenAsync(It.IsAny<OAuthRequest>()))
                 .ReturnsAsync(new TokenResponse
                 {
                     AccessToken = "TOKEN_FAKE",
@@ -30,6 +29,44 @@ namespace PopupBackgroundApp.Tests.Core
             var token = await service.ObterBearerTokenAsync();
 
             Assert.That(token, Is.EqualTo("TOKEN_FAKE"));
+        }
+
+        [Test]
+        public void ObterBearerTokenAsync_DeveLancarExcecao_QuandoTokenForVazio()
+        {
+            var authApiMock = new Mock<IAuthApi>();
+
+            authApiMock
+                .Setup(api => api.GerarTokenAsync(It.IsAny<OAuthRequest>()))
+                .ReturnsAsync(new TokenResponse
+                {
+                    AccessToken = string.Empty,
+                    ExpiresIn = 0
+                });
+
+            var service = new AuthService(authApiMock.Object);
+
+            Assert.ThrowsAsync<Exception>(async () =>
+            {
+                await service.ObterBearerTokenAsync();
+            });
+        }
+
+        [Test]
+        public void ObterBearerTokenAsync_DeveLancarExcecao_QuandoApiRetornarErro()
+        {
+            var authApiMock = new Mock<IAuthApi>();
+
+            authApiMock
+                .Setup(api => api.GerarTokenAsync(It.IsAny<OAuthRequest>()))
+                .ThrowsAsync(new Exception("401 Unauthorized"));
+
+            var service = new AuthService(authApiMock.Object);
+
+            Assert.ThrowsAsync<Exception>(async () =>
+            {
+                await service.ObterBearerTokenAsync();
+            });
         }
     }
 }
